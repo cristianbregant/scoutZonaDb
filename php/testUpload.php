4dffe63da9ac77ea21ace59aa312f1b590282e3c
Cristian Bregant <cristian.bregant94@gmail.com>
2017-03-08 17:46:56 +0100

    Fixes create folder upload
    Now if is the first time you upload a file it checks for the folder existence and if not it create them

--- php/testUpload.php
+++ php/testUpload.php
@@ -5,17 +5,14 @@ $uploadOk = 1;
 $desc = $_POST['description'];
 $branca = $_POST['branca'];
 $target_dir = "../uploads/".$branca."/";
+if (!file_exists($target_dir) {
+    mkdir($target_dir, 0777, true);
+}
+
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-   /* $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }*/
+
 }
 // Check if file already exists
 if (file_exists($target_file)) {
